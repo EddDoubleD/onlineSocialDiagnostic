bb4754ae68b8a346effdb07d88121723ce354f07
sulimov.dmitriy <sulimov.dmitriy@otr.ru>
2020-11-07 16:27:39 +0300

    fix java.lang.RuntimeException: Canvas: trying to draw too large

--- app/src/main/java/ru/hardwork/onlinesocialdiagnosticapp/MainActivity.java
+++ app/src/main/java/ru/hardwork/onlinesocialdiagnosticapp/MainActivity.java
@@ -1,5 +1,6 @@
 package ru.hardwork.onlinesocialdiagnosticapp;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.drawable.Drawable;
@@ -20,19 +21,19 @@ public class MainActivity extends AppCompatActivity {
 
     ImageView logo;
     GeometricProgressView progressView;
-    // GifImageView gifImageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        progressView = findViewById(R.id.progressView);
-        progressView.setColor(R.color.palePurple);
+       // progressView = findViewById(R.id.progressView);
+        //progressView.setColor(R.color.peach);
 
-        logo = findViewById(R.id.logoMain);
-        Drawable ic_logo_main = getDrawable(R.drawable.ic_logo_main);
-        logo.setImageDrawable(ic_logo_main);
+        //logo = findViewById(R.id.logoMain);
+        //@SuppressLint("UseCompatLoadingForDrawables")
+        //Drawable ic_logo_main = getDrawable(R.drawable.ic_logo_main);
+        //logo.setImageDrawable(ic_logo_main);
         //gifImageView = findViewById(R.id.gifLogo);
 
         final Intent homeActivity = new Intent(MainActivity.this, Home.class);
