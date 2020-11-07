ecd011c05c6c1c65388123d9052be5e9f623a0fb
sulimov.dmitriy <sulimov.dmitriy@otr.ru>
2020-11-07 21:24:02 +0300

    refactor Done.DecryptionViewModelFactory.class -> ru.hardwork.onlinesocialdiagnosticapp.factory.DecryptionViewModelFactory

--- app/src/main/java/ru/hardwork/onlinesocialdiagnosticapp/Done.java
+++ app/src/main/java/ru/hardwork/onlinesocialdiagnosticapp/Done.java
@@ -7,7 +7,6 @@ import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.text.Html;
 import android.text.method.LinkMovementMethod;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -29,8 +28,8 @@ import java.util.List;
 import ru.hardwork.onlinesocialdiagnosticapp.application.OnlineSocialDiagnosticApp;
 import ru.hardwork.onlinesocialdiagnosticapp.common.Common;
 import ru.hardwork.onlinesocialdiagnosticapp.common.lite.DiagnosticContract;
+import ru.hardwork.onlinesocialdiagnosticapp.factory.DecryptionViewModelFactory;
 import ru.hardwork.onlinesocialdiagnosticapp.factory.DescriptionViewModel;
-import ru.hardwork.onlinesocialdiagnosticapp.factory.IFactory;
 import ru.hardwork.onlinesocialdiagnosticapp.holders.DescriptionViewHolder;
 import ru.hardwork.onlinesocialdiagnosticapp.model.diagnostic.Decryption;
 
@@ -41,7 +40,7 @@ public class Done extends AppCompatActivity {
 
     private static final String BASE_FORMAT = "yyyy.MM.dd HH:mm";
     private static final String RESULT = "RESULT";
-    private static final String SPLITTER = ",";
+
     private static final String HTML = "<p><a href=\"%s\">Расшифровка теста</a></p>";
     @SuppressLint("SimpleDateFormat")
     private static final SimpleDateFormat FORMAT = new SimpleDateFormat(BASE_FORMAT);
@@ -91,82 +90,10 @@ public class Done extends AppCompatActivity {
             resultText.setText(Html.fromHtml(format(HTML, decryption.getUrl())));
             resultText.setLinksClickable(true);
             resultText.setMovementMethod(LinkMovementMethod.getInstance());
-            ViewModelFactory factory = new ViewModelFactory(result, decryption);
+            DecryptionViewModelFactory factory = new DecryptionViewModelFactory(result, decryption);
             List<DescriptionViewModel> desc = factory.build();
             DecryptionAdapter adapter = new DecryptionAdapter(desc);
             mRecyclerView.setAdapter(adapter);
-        }
-    }
-
-    static class ViewModelFactory implements IFactory<DescriptionViewModel> {
-
-        private ArrayList<Integer> processing;
-        private Decryption decryption;
-
-        public ViewModelFactory() {
-
-        }
-
-        public ViewModelFactory(ArrayList<Integer> processing, Decryption decryption) {
-            this.processing = processing;
-            this.decryption = decryption;
-        }
-
-
-        public void setProcessing(ArrayList<Integer> processing) {
-            this.processing = processing;
-        }
-
-        public void setDecryption(Decryption decryption) {
-            this.decryption = decryption;
-        }
-
-        @Override
-        public List<DescriptionViewModel> build() {
-            List<DescriptionViewModel> result = new ArrayList<>();
-            // Получение шкал
-            for (Decryption.Accent accent : decryption.getAccents()) {
-                int semi = 0;
-                int max = 0;
-                String[] positive = StringUtils.split(accent.getPositive(), SPLITTER);
-                if (positive != null) {
-                    for (String s : positive) {
-                        int i = Integer.parseInt(StringUtils.trim(s)) - 1;
-                        if (processing.size() <= i) {
-                            Log.e("Done:", "Не найден вариант для метрики");
-                            continue;
-                        }
-
-                        if (processing.get(i) == 1) {
-                            semi++;
-                        }
-                        max++;
-                    }
-                }
-                // Вычисляем из отрицательных ответов
-                String[] negative = StringUtils.split(accent.getNegative(), SPLITTER);
-                if (negative != null) {
-                    for (String s : negative) {
-                        int i = Integer.parseInt(StringUtils.trim(s)) - 1;
-                        if (processing.size() <= i) {
-                            Log.e("Done:", "Не найден вариант для метрики");
-                            continue;
-                        }
-
-                        if (processing.get(i) == 0) {
-                            semi++;
-                        }
-                        max++;
-                    }
-                }
-                // Получаем нормализованное значение
-                semi *= accent.getMultiple();
-                max *= accent.getMultiple();
-                DescriptionViewModel desc = new DescriptionViewModel(accent.getName(), max, semi);
-                result.add(desc);
-            }
-
-            return result;
         }
     }
 
